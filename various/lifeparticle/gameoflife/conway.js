e07ce13349aa5ca92e95ff1411fb42bde3179568
Kaminoatowo <j.cocomello@gmail.com>
2023-10-27 15:49:55 +0200

    edit conway js

--- various/lifeparticle/gameoflife/conway.js
+++ various/lifeparticle/gameoflife/conway.js
@@ -195,6 +195,34 @@ updateView = () => {
     }
 }
 
+function setupControlButtons() {
+    // button to start
+    var startButton = document.getElementById('start');
+    startButton.onclick = startButtonHandler;
+    
+    // button to clear
+    var clearButton = document.getElementById('clear');
+    clearButton.onclick = clearButtonHandler;
+    
+    // button to set random initial state
+    var randomButton = document.getElementById("random");
+    randomButton.onclick = randomButtonHandler;
+}
+
+function randomButtonHandler() {
+    if (playing) return;
+    clearButtonHandler();
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var isLive = Math.round(Math.random());
+            if (isLive == 1) {
+                var cell = document.getElementById(i + "_" + j);
+                cell.setAttribute("class", "live");
+                grid[i][j] = 1;
+            }
+        }
+    }
+}
 
 clearButtonHandler = () => {
     console.log("Clear the game: stop playing, clear the grid")
