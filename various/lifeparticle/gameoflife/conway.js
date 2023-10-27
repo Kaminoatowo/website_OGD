45f76c60516218634dac8d1fa1b03d9273ac7fe9
Kaminoatowo <j.cocomello@gmail.com>
2023-10-27 15:49:55 +0200

    update conway

--- various/lifeparticle/gameoflife/conway.js
+++ various/lifeparticle/gameoflife/conway.js
@@ -28,6 +28,61 @@ const reproductionTime = 100
         }
     }
 }*/
+
+// RULES
+// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any live cell with more than three live neighbours dies, as if by overcrowding.
+// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+function applyRules(row, col) {
+    var numNeighbors = countNeighbors(row, col);
+    if (grid[row][col] == 1) {
+        if (numNeighbors < 2) {
+            nextGrid[row][col] = 0;
+        } else if (numNeighbors == 2 || numNeighbors == 3) {
+            nextGrid[row][col] = 1;
+        } else if (numNeighbors > 3) {
+            nextGrid[row][col] = 0;
+        }
+    } else if (grid[row][col] == 0) {
+            if (numNeighbors == 3) {
+                nextGrid[row][col] = 1;
+            }
+        }
+    }
+    
+function countNeighbors(row, col) {
+    var count = 0;
+    if (row-1 >= 0) {
+        if (grid[row-1][col] == 1) count++;
+    }
+    if (row-1 >= 0 && col-1 >= 0) {
+        if (grid[row-1][col-1] == 1) count++;
+    }
+    if (row-1 >= 0 && col+1 < cols) {
+        if (grid[row-1][col+1] == 1) count++;
+    }
+    if (col-1 >= 0) {
+        if (grid[row][col-1] == 1) count++;
+    }
+    if (col+1 < cols) {
+        if (grid[row][col+1] == 1) count++;
+    }
+    if (row+1 < rows) {
+        if (grid[row+1][col] == 1) count++;
+    }
+    if (row+1 < rows && col-1 >= 0) {
+        if (grid[row+1][col-1] == 1) count++;
+    }
+    if (row+1 < rows && col+1 < cols) {
+        if (grid[row+1][col+1] == 1) count++;
+    }
+    return count;
+}
+
+// Start everything
+window.onload = initialize;
 // ...**** PARTICLES ****
 
 // **** VARIOUS ****...
